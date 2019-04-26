f6b14a2459e7f8b661a7612274b37c00587bbe5e
Stefan Irndorfer <stefan.irndorfer@gmail.com>
2019-04-26 21:37:41 +0200

    Added some real jokes.

--- jokes/src/main/java/stefanirndorfer/at/jokes/Joker.java
+++ jokes/src/main/java/stefanirndorfer/at/jokes/Joker.java
@@ -4,9 +4,23 @@ import java.util.Random;
 
 public class Joker {
     private final String[] jokes = {
-            "This is a funny joke",
-            "This is also a funny joke",
-            "And this one too!"
+            "Let me google this for you: https://short-funny.com/",
+            "Doctor: \"I'm sorry but you suffer from a terminal illness and have only 10 to live.\"\n" +
+                    "\n" +
+                    "Patient: \"What do you mean, 10? 10 what? Months? Weeks?!\"\n" +
+                    "\n" +
+                    "Doctor: \"Nine.\"",
+            "My old aunts would come and tease me at weddings, “Well Sarah? Do you think you’ll be next?” \n" +
+                    "-\n" +
+                    "We’ve settled this quickly once I’ve started doing the same to them at funerals.",
+            "A doctor accidentally prescribes his patient a laxative instead of a coughing syrup.\n" +
+                    "-\n" +
+                    "Three days later the patient comes for a check-up and the doctor asks: “Well? Are you still coughing?”\n" +
+                    "-\n" +
+                    "The patient replies: “No. I’m afraid to.” ",
+            "Job interviewer: “And where would you see yourself in five years’ time Mr. Jeffries?\"\n" +
+                    "-\n" +
+                    "Mr. Jeffries: \"Personally I believe my biggest weakness is in listening.\""
     };
 
     public String getJoke() {
